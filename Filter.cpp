59d4a41ccfec5f6813484f2b07dd1f4c51914c37
Adit Sachde <23707194+aditsachde@users.noreply.github.com>
2022-04-18 15:22:28 -0400

    add filtering step

--- Filter.cpp
+++ Filter.cpp
@@ -6,4 +6,13 @@ void Filter::step(float ax, float ay, float az, float gx, float gy, float gz) {
   // TODO Finish implementation
   Quaternion accel(0, ax, ay, az);
   accel.normalize();
+  Quaternion step = state.clone();
+
+  // step = 0.5 * current ⊗ accel
+  step.scale(0.5);
+  step.mult(accel);
+
+  // next = current + time delta * step
+  step.scale(TS_MS / (float) 1000);
+  state.add(step);
 }
